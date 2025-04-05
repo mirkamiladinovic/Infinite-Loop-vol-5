2f7fd0d2b52cb4b425c00bcb5e40390b8ad637fe
xor525 <bgias81290asjnfancoj@proton.me>
2025-04-05 15:43:11 +0200

    switching from C to Python + Andrej tkinter

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vega
+## A Python logger file parser, written in Python.
+
+### How to use
+
+### Credits
+- Authors: xor525, Andrejj005
+
---
+++ src/vega.py
@@ -0,0 +1,96 @@
+#import tkinter as tk
+#from tkinter import ttk
+#import threading
+import time
+
+"""
+- Authors: xor525 (class, logic), Andrejj005 (tkinter)
+- Date: 05/4/2025
+- src/vega.py
+"""
+
+class Vega:
+    __slots__ = 'file'
+    
+    def __init__(self, file):
+        self.file = file
+
+    def pisanje_errora(self) -> None:
+        #def task() -> None:
+        error_lines = [line for line in self.file if "ERROR" in line]
+        #label_status.config(text="Pisanje ERROR logova...")
+
+        with open("error.txt", "w") as err_file:
+            for err in error_lines:
+                err_file.write(err)
+                #root.update_idletasks()
+
+        #threading.Thread(target=task).start()
+
+    def brojanje_logova(self) -> None:
+        counts = {
+            "ERROR": 0,
+            "WARNING": 0,
+            "INFO": 0,
+            "DEBUG": 0
+        }
+
+        for line in self.file:
+            for key in counts:
+                if key in line:
+                    counts[key] += 1
+                    break
+
+        print(f"""Ukupno logova: {sum(counts.values())}
+Error: {counts["ERROR"]}
+Warning: {counts["WARNING"]}
+Info: {counts["INFO"]}
+Debug: {counts["DEBUG"]}""")
+
+        #label_total.config(text=f"Ukupno logova: {total}")
+        #label_error.config(text=f"ERROR: {counts['ERROR']}")
+        #label_warn.config(text=f"WARNING: {counts['WARNING']}")
+        #label_info.config(text=f"INFO: {counts['INFO']}")
+        #label_debug.config(text=f"DEBUG: {counts['DEBUG']}")
+
+def main() -> None:
+    with open("../vegini_logovi.txt", "r") as logs:
+        file = logs.readlines()
+    vega = Vega(file)
+    vega.brojanje_logova()
+    vega.pisanje_errora()
+
+if __name__ == "__main__":
+    main()
+
+#root = tk.Tk()
+#root.geometry("500x500")
+#root.title("Vega Logovi GUI")
+
+#tk.Label(root, text="Vega Log Analizator", font=("Helvetica", 16, "bold")).pack(pady=10)
+
+#tk.Button(root, text="Pisanje ERROR-a u file", command=pisanje_errora, width=30).pack(pady=5)
+#tk.Button(root, text="Brojanje logova", command=brojanje_logova, width=30).pack(pady=5)
+
+#label_total = tk.Label(root, text="Ukupno logova: 0")
+#label_total.pack()
+
+#label_error = tk.Label(root, text="ERROR: 0")
+#label_error.pack()
+
+#label_warn = tk.Label(root, text="WARNING: 0")
+#label_warn.pack()
+
+#label_info = tk.Label(root, text="INFO: 0")
+#label_info.pack()
+
+#label_debug = tk.Label(root, text="DEBUG: 0")
+#label_debug.pack()
+
+#progress_bar = ttk.Progressbar(root, length=400)
+#progress_bar.pack(pady=15)
+
+#label_status = tk.Label(root, text="")
+#label_status.pack()
+
+#root.mainloop()
